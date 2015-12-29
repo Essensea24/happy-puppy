c3811938d0ed500a4a832690611971aa09c2558f
Essensea24 <sophie.chor24@gmail.com>
2015-12-29 14:45:52 -0800

    added background image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,16 +15,28 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-
 var $window = $(window);
+
+$(document).ready(function(){
+    if ($(window).width() <768) {
+        $('section.module.parallax').css("display", "none");
+        
+    }
+
+    if ($(window).width() > 768) {
+        $('section.module.parallax').show();
+    }
+})
+
+
+
 
 $(window).on('scroll', function() {
 
  // to remove logo after passing div.mission
     $topOffset = $(this).scrollTop();
-    console.log($topOffset);
  
-    if (($topOffset >= 1088) && (($window).width() >= 768)) {
+    if (($topOffset >= 1088) && ($(window).width() >= 768)) {
     	// $('nav.navbar-inverse').css("background-color", "black");
         $('img.brandword').fadeOut();
         $("img.phone-logo").hide();
@@ -55,11 +67,10 @@ $(window).on('scroll', function() {
 
 $(document).ready(function(){
 
-
 	$(".slideshow > div:gt(0)").hide();
 
 	setInterval(function() { 
-	  $('.slideshow > div:first')
+	   $('.slideshow > div:first')
 	    .fadeOut(7000)
 	    .next()
 	    .fadeIn(2000)
@@ -68,9 +79,9 @@ $(document).ready(function(){
 	},  7000);
 
 
-        $("button.navbar-toggle").click(function() {
-            $("img.brandword").hide();
-        });
+            $("button.navbar-toggle").click(function() {
+                $("img.brandword").hide();
+            });
 
      
 
