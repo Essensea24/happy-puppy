c435601d8e16d9d09355f39c5fa76a1f4bb58bd6
Essensea24 <sophie.chor24@gmail.com>
2016-01-05 10:14:58 -0800

    fixed double logos display on ipad-view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,69 +17,116 @@
 //= require bootstrap-sprockets
 var $window = $(window);
 
-
 // background image to load differently on smaller devices
 $(document).ready(function(){
-    if ($(window).width() <=1024) {
+
+
+    if ($(window).width() < 768) {
+        //hide the regular logo in nav
+        $('img.brandword').hide();
+        //show mobile logo with menu bar
+        $('img.phone-logo').show();
+        // hide parallax div since it won't work on tablet or mobile devices
         $('section.module.parallax').css("display", "none");
+
+        $(window).on('scroll', function() {
+            $topOffset = $(this).scrollTop();
+            console.log($topOffset );
+             //fade out phone logo when user see the footer logo
+            if ($topOffset > 470) {
+     
+                $('img.phone-logo').fadeOut('slow');
+                $('button.navbar-toggle').css({
+                    'float': 'left',
+                    'margin-left': '5%'
+                    });
+
+            } 
+            //fade in when user scrow backup away from the footer
+            if ($topOffset <= 470) {
+
+                $('img.phone-logo').fadeIn();
+                $('button.navbar-toggle').css("float", "right");
+            }
+        });
+    }
+
+    if ($(window).width() == 768) {
+        $('img.phone.logo').hide();
+        $('img.brandword').show();
+        // hide parallax div since it won't work on tablet or mobile devices
+        $('section.module.parallax').css("display", "none");
+
+         $(window).on('scroll', function() {
+            $topOffset = $(this).scrollTop();
+            console.log($topOffset );
+             //fade out phone logo when user see the footer logo
+            if ($topOffset > 470) {
+     
+                $('img.brandword').fadeOut('slow');
+
+            } 
+            //fade in when user scrow backup away from the footer
+            if ($topOffset <= 470) {
+
+                $('img.brandword').fadeIn();
+            }
+        });
+
+    }
+
+
+    if ($(window).width() ==1024) {
+        // hide parallax div since it won't work on tablet or mobile devices
+        $('section.module.parallax').css("display", "none");
+        //display background div for normal scroll background image
         $('div.mobile-background').show();
+        //click event listener to add empty div when menu button is hit
         $('button.navbar-toggle').click(function(){
             $('div.empty-div').toggle();
+
         })
+
+         $(window).on('scroll', function() {
+            $topOffset = $(this).scrollTop();
+            console.log($topOffset );
+             //fade out phone logo when user see the footer logo
+            if ($topOffset > 2600) {
+     
+                $('img.brandword').fadeOut('slow');
+
+            } 
+            //fade in when user scrow backup away from the footer
+            if ($topOffset <= 2600) {
+
+                $('img.brandword').fadeIn();
+            }
+        });
     }
 
     if ($(window).width() > 1024) {
         $('section.module.parallax').show();
         $('div.mobile-background').hide();
+
+        $(window).on('scroll', function() {
+            $topOffset = $(this).scrollTop();
+            console.log($topOffset );
+             //fade out phone logo when user see the footer logo
+            if ($topOffset > 1100) {
+     
+                $('img.brandword').fadeOut('slow');
+
+            } 
+            //fade in when user scrow backup away from the footer
+            if ($topOffset <= 1100) {
+
+                $('img.brandword').fadeIn();
+            }
+        });
+
     }
-
-
 })
 
-
-
-
-$(window).on('scroll', function() {
-
- // to remove logo after passing div.mission
-    $topOffset = $(this).scrollTop();
-    // console.log($topOffset);
- 
-    if (($topOffset >= 1088) && ($(window).width() > 768)) {
-        // $('nav.navbar-inverse').css("background-color", "black");
-        $('img.brandword').fadeOut();
-        $("img.phone-logo").fadeOut("slow");
-        $('ul.nav.navbar-nav').css("margin-left", "0px");
-           
-
-    }
-
-    if(($topOffset < 1088) && ($(window).width() > 768)) {
-        // $('nav.navbar-inverse').css("background-color", "white");
-        $('img.brandword').fadeIn();
-        $('ul.nav.navbar-nav').css("margin-left", "15%");
-            
-
-    } 
-
-    if(($topOffset > 470) && ($(window).width() <= 768)) {
-        // $('nav.navbar-inverse').css("background-color", "white");
-        $('img.phone-logo').fadeOut('slow');
-        $('button.navbar-toggle').css({
-            'float': 'left',
-            'margin-left': '5%'
-            });
-
-    } 
-
-     if(($topOffset <= 470) && ($(window).width() <= 768)) {
-        // $('nav.navbar-inverse').css("background-color", "white");
-        $('img.phone-logo').fadeIn();
-        $('button.navbar-toggle').css("float", "right");
-    }
-
-
-});
 
 
 
@@ -100,9 +147,7 @@ $(document).ready(function(){
     },  7000);
 
 
-            $("button.navbar-toggle").click(function() {
-                $("img.brandword").hide();
-            });
+       
 
      
 
